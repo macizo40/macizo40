84a8efdf1796d229a5f6c994f52de70ab541fbc4
Dainer Zavala <dainerz2002@yahoo.com.mx>
2025-07-24 13:27:47 -0600

    tuplas basic

---
+++ practice/tuplas.py
@@ -0,0 +1,8 @@
+#testing the current values that a tupla does have 
+
+tupla = (234,"text",[2,2,3,4,5,6],-50)
+
+print("Values of the tupla are:{}".format(tupla))
+
+#all values are inmutable so there is no way to change it
+

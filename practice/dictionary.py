ef393fab2122e8582879c0d4d83e360b60a6db31
Dainer Zavala <dainerz2002@yahoo.com.mx>
2025-07-29 18:40:10 -0600

    doing maths with dictionary

--- practice/dictionary.py
+++ practice/dictionary.py
@@ -27,6 +27,33 @@ myColors['yellow'] = 'Japanese'
 print ("the traduction is wrong for yellow since is {}".format(myColors['yellow']))
 
 
+#using the values also we can delete them with an instruction
+
+del(myColors['yellow'])
+
+print("now we can see tht the translation that was weird is not longer in the list {}".format(myColors))
+
+#we have been using the keys for numbers and values but now lets do the values in numbers and keys 
+
+salaries = {'Tom':2000,'Jeff':1000,'Carl':3000}
+
+print("now lets see the values in this new dictionary here {}".format(salaries))
+
+#now using the numbers we can modify the values of the key and gives them a new value as exmaple, lets increase 100 to Tom
+
+salaries['Tom']+=100
+
+print("now lets see the list of values again {}".format(salaries))
+
+#let play with math
+
+print("the total of money of all the people in the {} is {}".format(salaries,salaries['Carl']+salaries['Jeff']+salaries['Tom']))
+
+# finally the method items in a dict will return two values and in a for we can
+
+for employees,salary in salaries.items():
+    print("the employe is {} and his salary is {}".format(employees,salary))
+
 #now lets try to use the reverse instead of key lets find the value and see what gives us
 
 #print("now I will point our to the value amarillo and see if this work here {}".format(myColors['amarillo']))

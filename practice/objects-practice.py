a08c481531e8c0067eb5c7a7e5c32fc313d60328
Dainer Zavala <dainerz2002@yahoo.com.mx>
2025-09-15 16:16:14 -0600

    creating a new class

--- practice/objects-practice.py
+++ practice/objects-practice.py
@@ -4,26 +4,57 @@
 class Cake:
 
     #this like java we have a constructor method (called here init) that will assign the values recived in the call to their variables
-    def __init__(self, flavour, size, form):
+    def __init__(self,name,flavour, size, form):
         self.flavour = flavour
         self.size = size
         self.form = form
+        self.name = name
 
     #this method was new for me, this is a way to return to screen in format string=str the content of the object usually with print   
     def __str__(self):
-        return 'Cake details are {} {} {}'.format(self.flavour,self.size,self.form)
+        return 'Cake {} details are {} {} {}'.format(self.name,self.flavour,self.size,self.form)
     
 #now we can play with some characteristics of the cakes
 
-red_velvet = Cake ("Chocolat","Big","Square")
-addiction = Cake ("Strawberry","Big","Circle")
-carrot = Cake ("Carrot","Big","Triangle")
+red_velvet = Cake ("red velvet","Chocolat","Big","Square")
+addiction = Cake ("addiction","Strawberry","Big","Circle")
+carrot = Cake ("carrot","Carrot","Big","Triangle")
+
+
 
 #now lets use the __STR__ method:
-
 print(red_velvet)
 print(addiction)
 print(carrot)
 
+#now what can we do with the object in a cake store, first we need the class name 
 
+class CakeShop:
 
+    #same logic as before, lets get a list of objects that will be our constructor, it will get a list of cakes objects
+    def __init__(self,cakes=[]):
+        self.cakes = cakes
+
+    #image then what I want to know from an object, maybe his form, we need a method to get it, we will use any value
+    def show_cake(self, value=None):
+        if not self.cakes:
+            print("No cakes available.")
+            return None
+
+        if value is None:
+            print("No search value provided.")
+            return None
+
+        # Normalize value for case-insensitive comparison
+        search_value = str(value).lower()
+
+        for cake in self.cakes:
+            # Dynamically check attributes
+                for attr in ("name", "flavour", "size", "form"):
+                    attr_value = getattr(cake, attr, None)
+                    if attr_value is not None and str(attr_value).lower() == search_value:
+                        print(f"Match found by {attr}: {cake}")
+                    return cake
+
+        print(f"No client found matching '{value}'.")
+        return None   

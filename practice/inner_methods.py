69a5a4f2a5f6733b9ab5056439dc06bb6e1aa627
Dainer Zavala <dainerz2002@yahoo.com.mx>
2025-09-23 18:24:32 -0600

    special methods in class

---
+++ practice/inner_methods.py
@@ -0,0 +1,43 @@
+#lets work with some of the special methods that are reserved by default, some of them are very popular in most of the proggamming languages 
+
+#now lets use a previous example that at the beining it was not not clear for me but now is different
+
+class Automobile:
+
+    #the constructor of this will be things that we can classify, we need to pass yes self first.
+
+    def __init__(self,brand,year,color,model):
+        #lets assign the values to self
+        self.brand = brand
+        self.year = year
+        self.color = color
+        self.model = model
+        #lets add a text that will help to confirm that constructor has run based on the model
+        print("New automobile '{}' was created".format(model))
+
+    #now a new specila method as other know as garbage collector, we can have a destrcutor methid known as "del"
+    #this method always happens when the object is deleted during the program, running now will show ar the end of the program a deletion
+    def __del__(self):
+        print("We are deleting automobile '{}'".format(self.model))
+
+    #a method that was previously used was str we can redefine the method to be more custom
+
+    def __str__(self):
+        #we just sue a return
+        return "{} brand {} launched in {}".format(self.model,self.brand,self.year)
+
+forteHB = Automobile("kia",2022,"white","forte HB")
+
+#we can call the destrcutor by calling the methid del
+
+#del(forteHB)
+
+#using a simple str methid with a number
+
+print(str(10))
+
+#now with our object
+
+print(str(forteHB))
+
+        

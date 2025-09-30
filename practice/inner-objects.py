4c67aad36c0259c759997ded9ae99e763e57ca51
Dainer Zavala <dainerz2002@yahoo.com.mx>
2025-09-29 22:32:10 -0600

    inner classes

---
+++ practice/inner-objects.py
@@ -0,0 +1,32 @@
+#lets start to practice inner objects which will help to use more the poo
+#to start we need to think a set of objects that can be include in other objects and those need details
+
+class Anime:
+
+    def __init__(self,title,seasons,date_of_beginining):
+        self.title = title
+        self.seasons = seasons
+        self.date_of_beginining = date_of_beginining
+        print ("Anime record was created",self.title)
+
+    #lets override the method str
+
+    def __str__(self):
+        return "{} ({})".format(self.title,self.date_of_beginining)
+    
+#lets now play with have our catalog of objects
+
+class Catalog:
+    #this will be a global value that can be managed in the entire class
+    my_catalog = []
+
+    #lets create his constructor, all class need one
+
+    def __init__(self,catalog=[]):
+        self.my_catalog = catalog
+
+    #now lets create a method that will be saving our animes to that list
+    def add_anime(self,anime):
+        self.my_catalog.append(anime) 
+    
+

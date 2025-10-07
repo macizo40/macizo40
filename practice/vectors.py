4d0c5a3cd5b0c207d1b0f0106b1ff63fdcdd76bd
Dainer Zavala <dainerz2002@yahoo.com.mx>
2025-10-06 19:20:02 -0600

    vectors second part

--- practice/vectors.py
+++ practice/vectors.py
@@ -11,23 +11,43 @@ class Dot:
     
 
     def __str__(self):
-        print("Dot values ({},{})".format(self.x,self.y))
+        return("Dot values ({},{})".format(self.x,self.y))
 
-def cuadrant (Dot=None):
-    if Dot.x is True and Dot.y is True:
-        print ("Dot is in the first quadrant")
-    elif Dot.x is True and Dot.y is False:
-        print ("Dot in the second quadrant")
-    elif Dot.x is False and Dot.y is False:
-        print ("Dot is in the third quadrant")
-    elif Dot.x is False and Dot.y is True:
-        print ("Dot is in the fourth quadrant")    
+    def quadrant (self):
+        if self.x > 0 and self.y > 0:
+            print ("Dot is in the first quadrant")
+        elif self.x < 0 and self.y > 0:
+            print ("Dot in the second quadrant")
+        elif self.x < 0 and self.y < 0:
+            print ("Dot is in the third quadrant")
+        elif self.x > 0 and self.y < 0:
+            print ("Dot is in the fourth quadrant")
+        elif self.x == 0 and self.y == 0:
+            print ("Dot is in the origin")
 
+    def vector (self,d):
+        print("Vector of dots {} and {} is ({},{})".format(self,d,d.x-self.x,d.y-self.y))
 
-def vector (dot1=Dot,dot2=Dot):
-    print("Vector is ({},{})".format(dot2.x-dot1.x,dot2.y-dot1.y))
+    def distance (self,d):
+        r = math.sqrt((d.x-self.x)**2 + (d.y-self.y)**2)
+        print("Distance of dots {} and {} is ({})".format(self,d,r))
 
-        
+a = Dot(2,3)
+b = Dot(5,5)
+c = Dot(-3,-1)
+d = Dot()
+
+a.quadrant()
+c.quadrant()
+d.quadrant()
+
+a.vector(b)
+b.vector(a)
+
+a.distance(b)
+b.distance(a)
+
+            
 
         
 

3a3cdb80e2cb7878888f93164f96c435b0d681e8
Dainer Zavala <dainerz2002@yahoo.com.mx>
2025-10-20 18:12:29 -0600

    collections string special

--- practice/collections-methods.py
+++ practice/collections-methods.py
@@ -3,6 +3,10 @@
 my_string = "Hello I am a simple string, you can play with me"
 my_lower_case_string = "i am a lower case string"
 my_repeated_string = "this is a lower lower lower, with many many many, repeated words words words"
+my_cvs_text = "Helo,5,value,58687,special,comma,sepparated,values,with,no,spaces"
+my_spaces_text = "   this is a text with space and begining and the end     "
+my_characters_text = "------this is a text with characters and begining----end-----"
+my_text_to_replace = "My name name name name is repeated, also want to read only e"
 
 #method upper will change all the string to uppe cases
 print(my_string.upper())
@@ -51,4 +55,64 @@ print(my_string.isalpha()," ",my_string)
 
 print(my_lower_case_string.isalpha()," ",my_lower_case_string)
 
+#validating types or writing
+print(my_lower_case_string.istitle()," ",my_lower_case_string)
 
+
+print(my_lower_case_string.islower()," ",my_lower_case_string)
+
+print(my_lower_case_string.isupper()," ",my_lower_case_string)
+
+print(my_lower_case_string.isspace()," ",my_lower_case_string)
+
+#in another types of methods we can validate the starts of a string
+
+print(my_lower_case_string.startswith("I")," ",my_lower_case_string)
+
+#also with a word we can see if start or end
+
+print(my_string.startswith("Hello")," ",my_string)
+
+print(my_string.endswith("me")," ",my_string)
+
+#lets try now manage and change the string with more advanced methods
+
+#split method with every space will separate the words and will create a list
+
+print(my_string.split()," ",my_string)
+
+#or we can save it to a new variable
+
+my_split_list = my_string.split()
+#I can now add a new item in the list and the original string will not be affected
+my_split_list.append("bye")
+
+print(my_split_list," ",my_string)
+
+#another good stuff with split I can maybe want to create a list with the last item or the first one using an index value
+
+print(my_string.split()[0]," ",my_string) #index 0 the first item that finds before an space
+print(my_string.split()[-1]," ",my_string) #index -1 last item found after the last space
+
+#methid split is also well used to parse comma separated values and have a list, we need to specify the character to use also can be any 
+
+print(my_cvs_text.split(','))
+
+#on the other hand there is a process to add a character to split an string, as example this
+
+print(my_alphanum," is moved to "," ".join(my_alphanum)) #this will separate each character with an space
+
+#another useful methiod commnly used to remove spaces or characters from begining or end of string
+
+print(my_spaces_text," is moved to ",my_spaces_text.strip())
+
+
+print(my_characters_text," is moved to ",my_characters_text.strip('-')) #notice that if there are characters between words those are not removed
+
+#and one of the most common ones, replace, this will replace the character by another or can be used to remove too
+
+print(my_text_to_replace,"-is moved to-",my_text_to_replace.replace('a','10')) #this will replace all letters a with a 10
+
+print(my_text_to_replace,"-is moved to-",my_text_to_replace.replace('name','',2)) #this will remove the word name 2 times, leaves spaces
+
+print(my_text_to_replace,"-is moved to-",my_text_to_replace.replace(' name','',3)) #this will remove the word name 3 times an space at the begining of the word 

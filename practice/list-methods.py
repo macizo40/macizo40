c4b9d1c1e9c618df0fe609a6206cfabe4c92e712
Dainer Zavala <dainerz2002@yahoo.com.mx>
2025-10-22 18:46:17 -0600

    list methods second part

--- practice/list-methods.py
+++ practice/list-methods.py
@@ -3,6 +3,8 @@
 my_initial_list = [1,2,3,4,5,6]
 my_list_to_count = [1,1,1,1,1,'hola','hola','a','a','a']
 
+my_list_to_sort = [1,0,7,3,9,10,455,2,1000]
+
 #methods to add at the end
 my_initial_list.append(7)
 
@@ -40,3 +42,69 @@ print(my_list_to_count.count(1))
 #to find the first index of the first element found
 
 print(my_list_to_count.index('a'))
+
+#another way to add elements on this way to an specific location in the list you can use insert
+
+my_upper_list.insert(0,"0") #add on the begining
+
+print("the upper list content:",my_upper_list) 
+
+my_upper_list.insert(-1,"X") #added in before the last item
+
+print("the upper list content:",my_upper_list) 
+
+#another method used in some practice of stacks is the pop, this simulate taken the last element of the list out
+
+print("pop from the list",my_upper_list.pop())
+
+
+print("pop from begining the list",my_upper_list.pop(0))
+
+#now we can remove a value specific in the list with remove
+my_upper_list.remove('X')
+
+print("removing from the list",my_upper_list)
+
+#in the case that the letter of the element is repeated in the list, remove will do the first one that find 
+
+my_list_to_count.remove('a')
+
+
+print("removing from the list",my_list_to_count)
+
+# we can play to reverse the content with another method called reverse
+my_lower_list.reverse()
+print("Initial lower content:",my_lower_list) 
+
+#since reverse it can not be used for strings, we can play with some tricks in the list to reverse the string
+
+my_text = "987654321"
+
+#lest create a list
+
+my_inverted_list = list(my_text) #this split all the content at individual elements
+
+print(my_inverted_list)
+
+#lest do de reverse
+
+my_inverted_list.reverse()
+
+
+print(my_inverted_list)
+
+#now we can join but with no characters to have the list ready
+
+my_right_text = "".join(my_inverted_list)
+
+print(my_right_text)
+
+#a common method to do is the sort, which will be doing from the lowest to the higher
+my_list_to_sort.sort()
+print("sorthing this ascendent",my_list_to_sort)
+
+#now lets do the descendent sort this need to be done with a specific argument
+
+my_list_to_sort.sort(reverse=True)
+print("sorthing this descendent",my_list_to_sort)
+

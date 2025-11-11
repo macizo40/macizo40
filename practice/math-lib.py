5f35828d8201c6c30808810fa7aed23c354a27ef
Dainer Zavala <dainerz2002@yahoo.com.mx>
2025-11-10 22:20:05 -0600

    math libs one

---
+++ practice/math-lib.py
@@ -0,0 +1,15 @@
+#lets start with the math library to do some very cool calculations 
+
+#we have to import math
+
+import math
+
+pi = 3.141656
+
+print(f"we will use simple round method for {pi} that is {round(pi)}")
+print(f"but if we want to round up lets use 3.5 a different result is {round(3.5)}, it round up")
+
+#but now lets start using more math methods this like other to round down
+
+print(f"using the methid 'math.floor' on pi will give {math.floor(pi)}") #all items not matter what will round down
+

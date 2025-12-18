6a4e55cca9a9d4dd0f6ef43d45dbaf35547b8580
Dainer Zavala <dainerz2002@yahoo.com.mx>
2025-12-17 18:31:28 -0600

    my first entry

---
+++ practice/entry.py
@@ -0,0 +1,13 @@
+#now is time to move and start doing some entry fields in the graphic env
+
+#adding the regular import
+
+from tkinter import *
+root = Tk()
+
+#this class will let you add the text field mode
+my_entry = Entry(root)
+my_entry.pack()
+
+
+root.mainloop()

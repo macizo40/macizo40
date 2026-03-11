77cdaa180d97bc7319b38a4bc40616c6f6ea1ab3
Dainer Zavala <dainerz2002@yahoo.com.mx>
2026-03-10 18:09:51 -0600

    lambda advanced filter with objects

---
+++ practice/lambda-advanced.py
@@ -0,0 +1,34 @@
+#now lets use more the strong of the filter looking for objects there is where the real thing happens
+
+#lets work with class again
+
+class People:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return "{} with age {}".format(self.name,self.age)
+    
+
+#now lets create a new list with the people object
+
+people_list = [
+    People("Ukyo",25),
+    People("Ahomaru",15),
+    People("Galford",34),
+    People("Nim",78),
+    People("Nakuru",12),
+]
+
+#lets take a look about what it is in the list:
+print("people list is ",people_list)
+
+#now we should be able to do this change with a lambda to find underage people
+
+underage = filter(lambda person:person.age <18,people_list)
+
+#now lets loop in the list that it was returned
+
+for i in underage:
+    print(i)

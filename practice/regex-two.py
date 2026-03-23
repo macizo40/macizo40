3907e1ae6f8cf6c81810ed3bf3c3bf2cae418b43
Dainer Zavala <dainerz2002@yahoo.com.mx>
2026-03-23 17:58:51 -0600

    regex with range

--- practice/regex-two.py
+++ practice/regex-two.py
@@ -26,11 +26,32 @@ mylist_patters_to_search = ['star','staa']
 
 patterns_find(mylist_patters_to_search,mytext)
 
-#now lest start to use the meta character this is used to find none or more repetitions of the letter at its left
+#now lest start to use the meta character this is used to find zero or more repetitions of the letter at its left 
+# (0 or more, this can be 1 or many)
 
 newtext = "cmd command coommmand cooooooommand"
 
-new_filter_list = ['co','co*']
+new_filter_list = ['c','co*']
 
 patterns_find(new_filter_list,newtext)
 #the output of the previous command will give specific 'co' found three time and the second 4 times including the first one
+
+#there is another special character that will find at least 1 or more, this is the +
+
+plus_filter = ['co+']
+
+patterns_find(plus_filter,newtext)
+
+#the next special character is the ? is the none or one repetition of the letter at its left 
+
+question_filter = ['co?','co?mm']
+
+patterns_find(question_filter,newtext)
+
+#there is character that will let us know an specific repetitive number in string {} always at the left also we can use a range
+
+repetive_filter = ['co{2}','co{2}m','co{0,5}m']
+
+patterns_find(repetive_filter,newtext)
+
+
